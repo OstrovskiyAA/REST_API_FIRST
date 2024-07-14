eba19faab295e3cfc47ee6aa60a35002e624a3ac
Aleksei Ostrovskii <a.a.ostrovskiy@mail.ru>
2024-07-14 15:44:20 +0300

    Added attachments

--- test_api_first.py
+++ test_api_first.py
@@ -3,6 +3,8 @@ import time
 
 import allure
 import requests
+from allure_commons.model2 import Attachment
+from allure_commons.types import AttachmentType
 from jsonschema import validate
 from selene import browser, have, by
 
@@ -49,7 +51,10 @@ def test_api_from_file_2():
         validate(body, schema=json.loads(file.read()))
 
 def test_api_from_variable_2():
-    response = requests.get(url_2)
+    with allure.step("Api request"):
+        response = requests.get(url_2)
+        allure.attach(body=response.text, name="Response", attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON, extension="json")
     print(response.text)
     body = response.json()
     assert body["data"]["name"] == "fuchsia rose"
@@ -75,6 +80,8 @@ def test_authorization_by_api():
         browser.open("https://demowebshop.tricentis.com/")
     with allure.step("Authorization by API"):
         response = requests.post(url_3, data=payload_3, allow_redirects=False)
+        allure.attach(body=response.text, name="Response", attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=str(response.cookies), name="Cookies", attachment_type=AttachmentType.TEXT, extension="txt")
     print(response.text)
     print(response.cookies)
     print(response.status_code)
@@ -87,3 +94,12 @@ def test_authorization_by_api():
     with allure.step("Assertion in terms of registration"):
         browser.all(".account").first.should(have.exact_text("a.a.ostrovskiy@mail.ru"))
     time.sleep(5)
+
+def reqres_api_get(url, **kwargs):
+    with allure.step("API Request"):
+        response=requests.get(url=url, **kwargs)
+        allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True),
+                      name="Response", attachment_type=AttachmentType.JSON, extension="json")
+
+def test_new():
+    reqres_api_get("https://reqres.in/api/unknown/2")
